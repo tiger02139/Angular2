82f2955ef85063ef2b708e70cb799d43587d39a4
skim_brk <skim@cambridgeassociates.com>
2017-11-20 16:19:31 -0500

    added installation notes

--- Labs/lab_Hello_Angular/src/app/app.component.ts
+++ Labs/lab_Hello_Angular/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome to Angular!';
-  firstName = "Sander";
+  firstName = "Hermione";
   birthDate : Date = new Date();
 }
